a324f2feb13a35facba3eb9d0997b086a37bcfef
Taj <singh_taj@hotmail.fr>
2019-09-23 13:43:16 +0200

    conditions

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 var port = process.env.PORT || 3000
 
+app.use(express.json())
+
 app.get('/', function (req, res) { res.send('Hello World!')
 })
 
 app.post('/chat', (req, res) => {
-  res.send('chatbot')
+  if(req.body.msg == "ville"){
+    let ville = req.body.msg
+    console.log("Nous sommes à Paris")
+    res.send(ville)
+  } else if (req.body.msg == "météo") {
+    meteo = req.body.msg
+    console.log("Il fait beau")
+    res.send(meteo)
+  }
 })
 
 app.listen(port, function () {
