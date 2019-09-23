d82fc2e4db6b7d0eefb4a5f95be77fccca0df1c5
Taj <singh_taj@hotmail.fr>
2019-09-23 12:00:47 +0200

    Ajout entree chat

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
-var port = process.env.PORT || 30000
+var port = process.env.PORT || 3000
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get('/', function (req, res) { res.send('Hello World!')
+})
+
+app.get('/chat', (req, res) => {
+  res.send('chat')
 })
 
 app.listen(port, function () {
